b064e9cbd91ed1448f8c09e5075953e2f781bbf5
Ritzahr <protocs123@gmail.com>
2024-10-17 16:50:43 +0100

    Carousel blur added

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -6,16 +6,19 @@ import { players } from "@/assets/players-2024/roster";
 const PlayerCarousel = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.inner}>
-        {players.map((player)=>{
-          return <PlayerCard 
-            name={`${player.name}`} 
-            position={`${player.position}`} 
-            img={player.image}
-            key={`${player.name}-${player.image}`} 
-        />
-      })
-      }
+      <div className={styles.gradientBottom}>
+        <div className={styles.inner}>
+           {players.map((player)=>{
+             return <PlayerCard 
+             name={`${player.name}`} 
+             position={`${player.position}`} 
+             img={player.image}
+             key={`${player.name}-${player.image}`} 
+             />
+            })
+          }
+          </div>
+        <div className={styles.gradientTop}></div>
       </div>
     </div>
   );
