53aac040c6c9f7f7e0aa8ced5a6a0979dfe92152
Ritzahr <protocs123@gmail.com>
2024-10-24 09:13:26 +0100

    Hologram styling

--- src/assets/images/full-body/index.js
+++ src/assets/images/full-body/index.js
@@ -1,7 +1,56 @@
-import alissonbeckerFBIMG from "./alisson-becker-profile-body-shot-202425.webp"
-import vitezslavjarosFBIMG from "./vitezslav-jaros-bodyshot-202425.webp"
+import alissonbeckerFBIMG from "./alisson-becker-profile-body-shot-202425.webp";
+import caoimhinkelleherFBIMG from "./caoimhin-kelleher-profile-body-shot-202425.webp";
+import vitezslavjarosFBIMG from "./vitezslav-jaros-bodyshot-202425.webp";
+
+import virgilvandijkFBIMG from "./virgil-van-dijk-profile-body-shot-202425.webp";
+import joegomezFBIMG from "./joe-gomez-profile-body-shot-202425.webp";
+import ibrahimakonateFBIMG from "./ibrahima-konate-body-shot-202425.webp";
+import kostastsimikasFBIMG from "./kostas-tsimikas-body-shot-202425.webp";
+import andyrobertsonFBIMG from "./andy-robertson-body-shot-202425.webp";
+import trentalexanderarnoldFBIMG from "./trent-alexander-arnold-profile-body-shot-202425.webp";
+import jarellquansahFBIMG from "./jarell-quansah-profile-body-shot-202425.webp";
+import conorbradleyFBIMG from "./conor-bradley-profile-body-shot-202425.webp";
+
+import dominikszoboszlaiFBIMG from "./dominik-szoboszlai-profile-body-shot-202425.webp";
+import alexismacallisterFBIMG from "./alexis-mac-allister-profile-body-shot-202425.webp";
+import curtisjonesFBIMG from "./curtis-jones-profile-body-shot-202425.webp";
+import harveyelliottFBIMG from "./harvey-elliott-profile-body-shot-202425.webp";
+import wataruendoFBIMG from "./wataru-endo-profile-body-shot-202425.webp";
+import ryangravenberchFBIMG from "./ryan-gravenberch-profile-body-shot-202425.webp";
+import tylermortonFBIMG from "./tyler-morton-bodyshot-202425.webp";
+
+import luisdiazFBIMG from "./luis-diaz-profile-body-shot-202425.webp";
+import darwinnunezFBIMG from "./darwin-nunez-profile-body-shot-202425.webp";
+import mohamedsalahFBIMG from "./mohamed-salah-bodyshot-v2-2024.webp";
+import codygakpoFBIMG from "./cody-gakpo-profile-body-shot-202425.webp";
+import diogojotaFBIMG from "./diogo-jota-profile-action-shot-202425.webp";
+import federicochiesaFBIMG from "./federico-chiesa-bodyshot-new-202425.webp";
+
 
 export {
     alissonbeckerFBIMG,
-    vitezslavjarosFBIMG
-}
+    caoimhinkelleherFBIMG,
+    vitezslavjarosFBIMG,
+    virgilvandijkFBIMG,
+    joegomezFBIMG,
+    ibrahimakonateFBIMG,
+    kostastsimikasFBIMG,
+    andyrobertsonFBIMG,
+    trentalexanderarnoldFBIMG,
+    jarellquansahFBIMG,
+    conorbradleyFBIMG,
+    dominikszoboszlaiFBIMG,
+    alexismacallisterFBIMG,
+    curtisjonesFBIMG,
+    harveyelliottFBIMG,
+    wataruendoFBIMG,
+    ryangravenberchFBIMG,
+    tylermortonFBIMG,
+    luisdiazFBIMG,
+    darwinnunezFBIMG,
+    mohamedsalahFBIMG,
+    codygakpoFBIMG,
+    diogojotaFBIMG,
+    federicochiesaFBIMG
+  };
+  
--- src/components/molecules/carousel/carousel.js
+++ src/components/molecules/carousel/carousel.js
@@ -4,41 +4,45 @@ import { players } from "@/assets/players-2024/roster";
 import { useState } from "react";
 
 const PlayerCarousel = () => {
-  const [cardClicked, setCardClicked]= useState(false);
+  const [cardClicked, setCardClicked]= useState(players[0]);
   const [hologramIMG, sethologramIMG]= useState(null);
+  const [hiding, setHiding] = useState(false);
 
   
   const cardClick = (name) => { 
       setCardClicked(players.find((player) => player.name===name))
-      sethologramIMG(name)
-  };
-  console.log(cardClicked)
+      setHiding((current)=> {
+        console.log(current)
+        return !current
+      })
+  }
 
   return (
-    <div className={styles.wrapper}>
-      <div className={`${styles.hologram} ${cardClicked ? styles.hologramClicked : ''}`}>
-        {cardClicked && (<img src={`${cardClicked.bodyImage.src}`}/>)}
-      </div>
       <div className={styles.gradientTop}>
+    <div className={styles.wrapper}>
+      <div className={styles.hologram}>
+        {hiding && (<div className={`${styles.hiding}`}><img src={`${cardClicked.bodyImage.src}`}/></div> )}
+      </div>
         <div className={styles.inner}>
           <div className={styles.cardRow}>
             {players.map((player) => {
               return (
-                  <PlayerCard
-                    name={`${player.name}`}
-                    position={`${player.position}`}
-                    img={player.image}
-                    key={`${player.name}-${player.image}`}
-                    handleClick={cardClick}
-                  />
+                <PlayerCard
+                name={`${player.name}`}
+                position={`${player.position}`}
+                img={player.image}
+                key={`${player.name}-${player.image}`}
+                handleClick={cardClick}
+                />
               );
             })}
           </div>
+            <div className={styles.gradientBottom}></div>
           <div className={styles.arrows}>
             <div className={styles.arrowsContent}> *ARROWS HERE* </div>
           </div>
         </div>
-        <div className={styles.gradientBottom}></div>
+        
       </div>
     </div>
   );
