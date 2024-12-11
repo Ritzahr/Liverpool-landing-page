c56f560bdf8f80c95517fc4434a2466a431615da
Ritzahr <protocs123@gmail.com>
2024-12-11 16:20:37 +0000

    Footer created

---
+++ src/assets/icons/index.js
@@ -0,0 +1,8 @@
+import fbIcon from './facebook-logo-2428.svg';
+import xIcon from './twitter-logo-black-outline-20846.svg';
+import instaIcon from './instagram-logo-8869.svg';
+import blueIcon from './bluesky-blue-logo-24455.svg';
+
+const icons = [fbIcon, xIcon, instaIcon, blueIcon];
+
+export { icons };
