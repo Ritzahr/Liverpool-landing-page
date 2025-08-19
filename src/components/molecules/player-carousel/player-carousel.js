8a556dfdd988f6790d49febada4586ab4c373f09
Ritzahr <protocs123@gmail.com>
2025-08-19 11:16:57 +0100

    Refactoring for Mobile view

--- src/components/molecules/player-carousel/player-carousel.js
+++ src/components/molecules/player-carousel/player-carousel.js
@@ -40,7 +40,7 @@ const PlayerCarousel = () => {
       return () => clearTimeout(timer);
     }
   }, [fade]);
-  console.log(cardClicked)
+
   return (
     <main className={styles.section}>
       <div className={styles.hologramSection}>
@@ -60,7 +60,7 @@ const PlayerCarousel = () => {
           )}
       </div>
         <div 
-         className={styles.cardRow}
+         className={styles.container}
          ref={playerRef}
         >
           {players.map((player) => {
