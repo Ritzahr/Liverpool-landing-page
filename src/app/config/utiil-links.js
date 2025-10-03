8ef3dc36276c6e7dee2fadb1f77276a5dc452acf
Ritzahr <protocs123@gmail.com>
2025-10-03 16:53:58 +0100

    Fixed footer button stylings

---
+++ src/app/config/utiil-links.js
@@ -0,0 +1,25 @@
+const utilities = [
+  "About Us",
+  "Contact Us",
+  "mynewsassistant",
+  "RSS Feeds",
+  "Newsletter Signup",
+  "Syndication & Licensing",
+  "T&Cs",
+  "Cookie Policy",
+  "Competition Rules",
+  "How to Complain",
+  "Corrections & Clarifications",
+  "Privacy Notice",
+  "AI Notice",
+  "Do not sell or share my data",
+];
+
+const links = [
+  { text:"Liverpool FC News", link:"https://www.thisisanfield.com/liverpool-fc-news/" },
+  { text:"Shop", link:"https://store.liverpoolfc.com" },
+  { text:"Premier League", link:"http://premierleague.com/" },
+  { text:"Github", link:"https://github.com/Ritzahr" },
+]
+
+export { utilities, links };
