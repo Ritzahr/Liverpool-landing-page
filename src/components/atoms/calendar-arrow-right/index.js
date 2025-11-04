518db1b94fba6282e4e1702cde64514e09657e6b
Ritzahr <protocs123@gmail.com>
2025-11-04 21:31:55 +0000

    Intersection Observer added and onclick scaffolding added

--- src/components/atoms/calendar-arrow-right/index.js
+++ src/components/atoms/calendar-arrow-right/index.js
@@ -1,10 +1,18 @@
 import styles from "./calendar-arrow-right.module.css";
 import { ChevronDown } from "../chevron-down";
 
-const RightCalendarArrow = ({ clickedYear }) => (
-  <div className={styles.container}>
-    {clickedYear && <ChevronDown height={120} width={120} fill="white" />}
+const RightCalendarArrow = ({ clickedYear }) => {
+  const clickHandle = () => {
+    console.log("Click", clickedYear);
+  };
+
+  return (<div className={styles.container}>
+    <div className={styles.wrapper} onClick={clickHandle}>
+      {clickedYear && <ChevronDown height={120} width={120} fill="white" />}
+    </div>
   </div>
-);
+  )
+};
+
 
 export { RightCalendarArrow };
